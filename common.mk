938d71150244a59edb45672cf11a011146f78c28
GiaSen <giammasena@gmail.com>
2023-01-28 22:12:16 +0530

    sm8250-common: Drop GcamGo

--- common.mk
+++ common.mk
@@ -122,8 +122,6 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libstdc++.vendor
 
-PRODUCT_PACKAGES += \
-    GcamGo
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
