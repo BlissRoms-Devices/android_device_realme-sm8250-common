e2280b1076aed5fbeb87dca39899abf492af8f06
LuK1337 <priv.luk@gmail.com>
2024-09-09 11:22:23 +0200

    sm8250-common: Setup ELF checks
    Change-Id: I89154af89a7be70fa3702ac3a74adf8bd087b6ef
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -8,10 +8,56 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+    'device/realme/sm8250-common',
+    'hardware/oplus',
+    'hardware/qcom-caf/sm8250',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_vendor' if partition in ['odm', 'vendor'] else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    (
+        'com.qti.stats.pdlib',
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libhistogram',
+        'libmmosal',
+        'libsdedrm',
+        'libsdmcore',
+        'vendor.qti.imsrtpservice@3.0',
+        'libdisplayconfig.qti',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libgpu_tonemapper',
+        'libgrallocutils',
+        'libOmxCore',
+        'libOmxVenc',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
@@ -38,6 +84,9 @@ module = ExtractUtilsModule(
     'sm8250-common',
     'realme',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
 )
 
 if __name__ == '__main__':
