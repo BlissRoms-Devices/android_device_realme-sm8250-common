c81aeb8599ad67c4b50345b0dfc793448dca35d1
sreelekshman <sreelekshmanchunakara@gmail.com>
2025-05-10 07:32:39 +0000

    sm8250-common: Cleanup before adapting for LineageOS
    Revert "sm8250-common: build libcrypto_shim.vendor"
    
    This reverts commit e7294bece8b212ae0f6eb7023e6e824c5cc25e23.
    
    Revert "sm8250-common: Reorder proprietary-files.txt"
    
    This reverts commit 8ff2852d8ac4af1052317cb82ef4e10a0dc32c7d.
    
    Revert "sm8250-common: Update Adreno drivers to v744.12"
    
    This reverts commit 5f9d260ff92c1c17d1cc9b6af5cd5f510f921307.
    
    Revert "sm8250-common: Update radio and ims blobs from LA.QSSI.13.0.r1-09700-qssi.0"
    
    This reverts commit 3fc91fbfb93ecab1bd95673b86e242be6cb47499.
    
    Revert "sm8250-common: Add missing vibration firmware files"
    
    This reverts commit d156075e531b10ecae6fa59f9e11e4541ea5dd16.
    
    Revert "sm8250-common: Use extract-tools patchelf binary"
    
    This reverts commit c85c5ad973514a6962c42461cd66a4fb0ffa267f.
    
    Revert "sm8250-common: Patch more blobs to load libprotobuf-cpp-full-3.9.1"
    
    This reverts commit 08d56b2a969a478d16906156bace9481c120d0af.

--- common.mk
+++ common.mk
@@ -149,7 +149,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
-    libcrypto_shim.vendor
+    libcrypto_shim
 
 PRODUCT_PACKAGES += \
     gralloc.kona \
--- extract-files.sh
+++ extract-files.sh
@@ -70,7 +70,7 @@ function blob_fixup() {
             "${SIGSCAN}" -p "AF 0B 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib/libgui1_vendor.so)
-            "${PATCHELF}" --replace-needed "libui.so" "libui-v30.so" "${2}"
+            patchelf --replace-needed "libui.so" "libui-v30.so" "${2}"
             ;;
         odm/lib64/libwvhidl.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
@@ -78,44 +78,11 @@ function blob_fixup() {
         odm/lib64/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
-        odm/lib64/libdmtp.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        odm/lib64/libdmtpclient.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        odm/lib64/libdmtp-protos-lite.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        odm/lib64/lib-virtual-modem-protos.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        odm/lib64/liboplus_service.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
         odm/lib64/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
         odm/lib64/libwvhidl.so)
             "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
-            ;;
-        vendor/lib64/libssc.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        vendor/lib64/libsensorcal.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        vendor/lib64/sensors.ssc.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        vendor/lib64/libsnsdiaglog.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        vendor/lib64/libsnsapi.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
-        vendor/bin/sensors.qti)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
             ;;
         vendor/lib64/libwvhidl.so|vendor/lib/mediadrm/libwvdrmengine.so|vendor/lib64/mediadrm/libwvdrmengine.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
